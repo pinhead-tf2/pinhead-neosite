4fcc9747afb77176589c6806555262d2926e4ee1
pinhead <mrmaxc2023@gmail.com>
2023-05-08 20:39:00 -0500

    beginning restructure

--- random_quote_populator.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    const quotes = ["🏳️‍⚧️ rights", "hi computer nerds", "we stay silly", "door stuck", "go go gadget gamer time",
-        "karl would approve of this", "rock and stone", "praise ralsei", "diggy diggy hole", "the cyber grind",
-        "🫵🤨🏳️‍🌈?", "poot dispenser here", "that spy's a bloody medic", "medic", "google en passant", "holy hell"
-    ];
-    const quoteLabel = document.getElementById("random-quote");
-    quoteLabel.innerHTML = quotes[quotes.length*Math.random() | 0];
-});
