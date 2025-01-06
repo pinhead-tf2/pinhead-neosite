8b0af028b6ff7233fa69c076c14cd131086653ac
pinhead <pinhead@pinhead.dev>
2025-01-06 17:04:18 -0600

    Testing out svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+    <title>pinhead's svelte test</title>
+</svelte:head>
+
+<h1 class="m-0 py-4">Welcome to SvelteKit</h1>
+<p class="m-0 py-1">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
