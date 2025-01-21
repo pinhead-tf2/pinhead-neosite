8e2bdd8a0887d99174cf0e34fa13292f7d2a5739
pinhead <pinhead@pinhead.dev>
2025-01-20 23:54:46 -0600

    i got the navbar items centered

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
     import '../app.css';
+    import {page} from '$app/state';
+    import izumi from '$lib/images/izumi.png';
 
     let {children} = $props();
 
-    const pages = [["", "home"], ["about", "about"]];
+    const pages = [
+        {
+            name: "home",
+            path: "/"
+        },
+        {
+            name: "about",
+            path: "/about"
+        }
+    ]
 </script>
 
 <style>
@@ -29,6 +40,19 @@
             background-position: 100% 50%
         }
     }
+
+    .normal_page {
+        color: var(--ctp-mocha-subtext0-hex);
+    }
+
+    .normal_page:hover {
+        color: var(--ctp-mocha-blue-hex);
+    }
+
+    .current_page {
+        color: var(--header-current-color);
+        background-color: var(--header-current-background);
+    }
 </style>
 
 <div class="-z-10 content-none fixed w-screen h-screen"
@@ -36,17 +60,34 @@
 <div class="-z-10 content-none fixed w-[5000px] h-[5000px] origin-center overflow-clip" id="grid"></div>
 <div class="-z-10 content-none fixed w-screen h-screen backdrop-blur-sm"></div>
 
-<nav class="fixed flex items-center top-0 left-0 right-0 z-50 px-12 bg-[var(--ctp-mocha-mantle-hex)] h-14">
+<header class="fixed flex items-center justify-between top-0 left-0 right-0 z-50 px-6 bg-[var(--ctp-mocha-mantle-hex)] h-14">
     <!--    <a href="/" style="text-decoration: none !important;">-->
     <!--        <img alt="Izumi the slime hat" class="w-auto max-h-10 mr-1 py-2" src="{izumi}">-->
     <!--    </a>-->
-    <!--    <a href="/" style="text-decoration: none !important;">-->
-    <!--        <h1 class="m-0 mr-2 font-light text-xl">pinhead's site</h1>-->
-    <!--    </a>-->
-    {#each pages as page }
-        <a class="text-[var(--ctp-mocha-subtext0-hex)] hover:text-[var(--ctp-mocha-blue-hex)] transition-colors relative text-xl mx-1.5 h-full flex items-center"
-           href="/{page[0]}" style="text-decoration: none !important;">{page[1]}</a>
-    {/each}
-</nav>
+
+    <div class="flex-1">
+        <div class="flex items-center h-full">
+            <a href="/" style="text-decoration: none !important;">
+                <img alt="Izumi the slime hat" class="w-auto max-h-8 mr-1 py-2" src="{izumi}">
+            </a>
+            <a href="/" style="text-decoration: none !important;">
+                <h3 class="m-0 mr-2 font-light">pinhead's site</h3>
+            </a>
+        </div>
+    </div>
+    <div class="flex-1">
+        <nav class="md:flex justify-center items-center gap-4">
+            {#each pages as item }
+                <a class="relative items-center text-xl px-2 py-1.5 gap-1 rounded-lg no-underline transition ease-in-out hover:duration-200 {page.url.pathname === item.path ? 'current_page' : 'normal_page'}"
+                   href="{item.path}">{item.name}
+                </a>
+            {/each}
+        </nav>
+    </div>
+    <div class="flex-1 text-end">
+        <h4>extra stuff</h4>
+    </div>
+
+</header>
 
 {@render children()}
