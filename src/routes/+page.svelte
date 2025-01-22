62c5e20fe7a11b3d3711db3592fd1d61d1c8aa59
pinhead <pinhead@pinhead.dev>
2025-01-22 02:13:48 -0600

    small readability adjustments

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,12 +15,14 @@
             <RandomImage alt="A random artwork of one of pinhead's characters" classes="rounded-lg float-right max-w-64 bg-[#D7CEC9] my-6 ml-2"/>
             <h1 class="mb-1">Heya!</h1>
             <p>
-                I'm <span class="pinhead-text"><WavyText string="pinhead"/></span>, also known as <span class="pinhead-text"><WavyText string="Max" delayMultiplier="100"/></span>, and this is my personal site!
+                I'm <span class="pinhead-text inline-block"><WavyText string="pinhead"/></span>, also known as <span class="text-[#2196f3]">Max</span>, and this is my personal site!
                 I'm a <span class="text-[#9BCBEB]">Minnesotan</span> programmer who is currently studying <span class="text-[var(--ctp-mocha-maroon-hex)]">Computer Networking & Infrastructure Engineering</span>.
-                This site is a passion project of mine which I've been working on over the last year or two, and this marks my fourth iteration of it's design!
             </p>
             <p>
-                I don't exactly update this thing or my public projects too much, so if you really want to learn about me, I recommend you get in contact with me over <a href="https://discord.com/users/246291288775852033" class="text-[#7289da] visited:text-[#7289da]"><BouncingText string="Discord"/></a>!
+                This site is a passion project of mine which I've been working on over the last year or two, and this marks my fourth iteration of it's design. For this one, I'm really trying to push the limits of what I can do with random, fun ideas, like all this fancy animated text!
+            </p>
+            <p>
+                If really want to learn about me, I recommend you get in contact with me over <a href="https://discord.com/users/246291288775852033" class="text-[#7289da] inline-block visited:text-[#7289da]"><BouncingText string="Discord" classes="underline"/></a>!
             </p>
         </div>
     </div>
